fa5e2cd35fae3c5ab1aeb7aa6c9b4f09be4e6f03
Quantum1380 <1580493339@qq.com>
2026-04-14 04:07:33 +0800

    更改

--- backend/rag/ragtest.py
+++ backend/rag/ragtest.py
@@ -15,4 +15,4 @@ results = collection.query(
     query_texts=["This is a query document about hawaii"], # Chroma will embed this for you
     n_results=2 # how many results to return
 )
-print(results)
+print(results)  
